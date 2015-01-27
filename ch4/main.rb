1904118f506617bf677087b48a7b258fcb4ff6fd
joe <joe@mbPro.local>
2015-01-26 23:42:57 -0500

    add database configuration; chapter 4 complete

--- ch4/main.rb
+++ ch4/main.rb
@@ -1,9 +1,33 @@
 require 'sinatra'
+require 'sinatra/reloader' if development?
 require 'slim'
 require 'sass'
 require './song'
 
+configure do
+  enable :sessions
+  set :username, 'frank'
+  set :password, 'sinatra'
+end
+
+configure :development do
+  DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+end
+
+configure :production do
+  DataMapper.setup(:default, ENV['DATABASE_URL'])
+end
+
 get('/styles.css'){ scss :styles }
+
+# grab the name entered in the URL and store it in the params hash
+get '/set/:name' do
+  session[:name] = params[:name]
+end
+
+get '/get/hello' do
+  "Hello #{session[:name]}"
+end
 
 get '/' do
   slim :home
@@ -17,6 +41,24 @@ end
 get '/contact' do
   @title = "Contact"
   slim :contact
+end
+
+get '/login' do
+  slim :login
+end
+
+post '/login' do
+  if params[:username] == settings.username && params[:password] == settings.password
+    session[:admin] = true
+    redirect to('/songs')
+  else
+    slim :login
+  end
+end
+
+get '/logout' do
+  session.clear
+  redirect to('/login')
 end
 
 not_found do
--- ch4/song.rb
+++ ch4/song.rb
@@ -1,7 +1,9 @@
 require 'dm-core'
 require 'dm-migrations'
 
-DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+configure :development do
+  DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+end
 
 class Song
   include DataMapper::Resource
@@ -24,6 +26,7 @@ get '/songs' do
 end
 
 get '/songs/new' do
+  halt(401, 'Not Authorized') unless session[:admin]
   @song = Song.new
   slim :new_song
 end
@@ -34,22 +37,26 @@ get '/songs/:id' do
 end
 
 get '/songs/:id/edit' do
+  halt(401, 'Not Authorized') unless session[:admin]
   @song = Song.get(params[:id])
   slim :edit_song
 end
 
 post '/songs' do
+  halt(401, 'Not Authorized') unless session[:admin]
   song = Song.create(params[:song])
   redirect to("/songs/#{song.id}")
 end
 
 put '/songs/:id' do
+  halt(401, 'Not Authorized') unless session[:admin]
   song = Song.get(params[:id])
   song.update(params[:song])
   redirect to("/songs/#{song.id}")
 end
 
 delete '/songs/:id' do
+  halt(401, 'Not Authorized') unless session[:admin]
   Song.get(params[:id]).destroy
   redirect to('/songs')
 end
